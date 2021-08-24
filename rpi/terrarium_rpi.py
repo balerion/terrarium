5ddfec713b383dc11432e98e3ebbc5ad10c4e6f2
Davide Curcio <davide.curcio@gmail.com>
2021-08-24 11:43:18 +0200

    adding threading

--- rpi/terrarium_rpi.py
+++ rpi/terrarium_rpi.py
@@ -1,13 +1,43 @@
 # Importing Libraries
 import serial
 import time
-arduino = serial.Serial(port='/dev/ttyUSB0', baudrate=9600, timeout=.1)
+import threading
+
+port = '/dev/ttyUSB0'
+baud = 9600
+connected = False
+arduino = serial.Serial(port, baud, timeout=.1)
+
 def write_read(x):
     arduino.write(bytes(x, 'utf-8'))
     time.sleep(0.05)
     data = arduino.readline()
     return data
-while True:
+
+def handle_data(data):
+    print(data)
     num = input("Enter a number: ") # Taking input from user
     value = write_read(num)
     print(value) # printing the value
+
+def read_from_port(ser):
+    while not connected:
+        #serin = ser.read()
+        connected = True
+
+        while True:
+           print("test")
+           reading = ser.readline().decode()
+           handle_data(reading)
+
+thread = threading.Thread(target=read_from_port, args=(arduino,))
+thread.start()
+
+# while True:
+#     num = input("Enter a number: ") # Taking input from user
+#     value = write_read(num)
+#     print(value) # printing the value
+
+
+
+
