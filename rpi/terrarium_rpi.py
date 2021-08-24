e13f8404cc350010a45a6d52a6c15cabc14ca9db
Davide Curcio <davide.curcio@gmail.com>
2021-08-24 15:48:47 +0200

    adding serial com to rpi script

--- rpi/terrarium_rpi.py
+++ rpi/terrarium_rpi.py
@@ -1,12 +1,123 @@
-# Importing Libraries
+#!/usr/bin/env python
+'''
+A Python class implementing KBHIT, the standard keyboard-interrupt poller.
+Works transparently on Windows and Posix (Linux, Mac OS X).  Doesn't work
+with IDLE.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as 
+published by the Free Software Foundation, either version 3 of the 
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+'''
+
+import os
 import serial
 import time
-import threading
 
-port = '/dev/ttyUSB0'
-baud = 9600
-connected = False
-arduino = serial.Serial(port, baud, timeout=.1)
+
+
+arduino = serial.Serial(port='/dev/ttyUSB0', baudrate=9600, timeout=.1)
+
+
+
+# Windows
+if os.name == 'nt':
+    import msvcrt
+
+# Posix (Linux, OS X)
+else:
+    import sys
+    import termios
+    import atexit
+    from select import select
+
+
+class KBHit:
+
+    def __init__(self):
+        '''Creates a KBHit object that you can call to do various keyboard things.
+        '''
+
+        if os.name == 'nt':
+            pass
+
+        else:
+
+            # Save the terminal settings
+            self.fd = sys.stdin.fileno()
+            self.new_term = termios.tcgetattr(self.fd)
+            self.old_term = termios.tcgetattr(self.fd)
+
+            # New terminal setting unbuffered
+            self.new_term[3] = (self.new_term[3] & ~termios.ICANON & ~termios.ECHO)
+            termios.tcsetattr(self.fd, termios.TCSAFLUSH, self.new_term)
+
+            # Support normal-terminal reset at exit
+            atexit.register(self.set_normal_term)
+
+
+    def set_normal_term(self):
+        ''' Resets to normal terminal.  On Windows this is a no-op.
+        '''
+
+        if os.name == 'nt':
+            pass
+
+        else:
+            termios.tcsetattr(self.fd, termios.TCSAFLUSH, self.old_term)
+
+
+    def getch(self):
+        ''' Returns a keyboard character after kbhit() has been called.
+            Should not be called in the same program as getarrow().
+        '''
+
+        s = ''
+
+        if os.name == 'nt':
+            return msvcrt.getch().decode('utf-8')
+
+        else:
+            return sys.stdin.read(1)
+
+
+    def getarrow(self):
+        ''' Returns an arrow-key code after kbhit() has been called. Codes are
+        0 : up
+        1 : right
+        2 : down
+        3 : left
+        Should not be called in the same program as getch().
+        '''
+
+        if os.name == 'nt':
+            msvcrt.getch() # skip 0xE0
+            c = msvcrt.getch()
+            vals = [72, 77, 80, 75]
+
+        else:
+            c = sys.stdin.read(3)[2]
+            vals = [65, 67, 66, 68]
+
+        return vals.index(ord(c.decode('utf-8')))
+
+
+    def kbhit(self):
+        ''' Returns True if keyboard character was hit, False otherwise.
+        '''
+        if os.name == 'nt':
+            return msvcrt.kbhit()
+
+        else:
+            dr,dw,de = select([sys.stdin], [], [], 0)
+            return dr != []
+
 
 def write_read(x):
     arduino.write(bytes(x, 'utf-8'))
@@ -14,30 +125,35 @@ def write_read(x):
     data = arduino.readline()
     return data
 
-def handle_data(data):
-    print(data)
-    num = input("Enter a number: ") # Taking input from user
-    value = write_read(num)
-    print(value) # printing the value
+# Test    
+if __name__ == "__main__":
 
-def read_from_port(ser):
-    while not connected:
-        #serin = ser.read()
-        connected = True
+    kb = KBHit()
+    buffer = bytes()  # .read() returns bytes right?
 
-        while True:
-           print("test")
-           reading = ser.readline().decode()
-           handle_data(reading)
+    print('Hit any key, or ESC to exit')
 
-thread = threading.Thread(target=read_from_port, args=(arduino,))
-thread.start()
+    while True:
 
-# while True:
-#     num = input("Enter a number: ") # Taking input from user
-#     value = write_read(num)
-#     print(value) # printing the value
+        if kb.kbhit():
+            c = kb.getch()
+            if ord(c) == 27: # ESC
+                break
+            print(f"sending {c.encode('ascii')} to arduino")
+            arduino.write(c.encode('ascii'))
+            arduino.write(b'\n')
+
+        
+        if arduino.in_waiting > 0:
+            buffer += arduino.read(arduino.in_waiting)
+            try:
+                complete = buffer[:buffer.index(b':')+1]  # get up to '}'
+                buffer = buffer[buffer.index(b':')+1:]  # leave the rest in buffer
+            except ValueError:
+                continue  # Go back and keep reading
+            print('buffer=', complete)
+            ascii = buffer.decode('ascii')
+            print('ascii=', ascii)
 
 
-
-
+    kb.set_normal_term()
